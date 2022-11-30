096957b1fc0407836517a22c67074a4bf255647e
ChrisABrown <punkystylin@yahoo.com>
2022-11-30 12:50:00 -0600

    added hit count code and defined resources

--- lambda/hello.py
+++ lambda/hello.py
@@ -1,14 +1,12 @@
 from asyncio import events
 import json
-from urllib import request 
+from urllib import request
 
 
 def handler(event, context):
-    print('request: {}'.format(json.dumps(event)))
+    print("request: {}".format(json.dumps(event)))
     return {
-        'statusCode': 200,
-        'headers': {
-            'Content-Type': 'text/plain'
-            },
-        'body': 'Hello, CDK! You have hit {}\n'.format(event['path'])
-          }
+        "statusCode": 200,
+        "headers": {"Content-Type": "text/plain"},
+        "body": "Hello, CDK! You have hit {}\n".format(event["path"]),
+    }
--- py_cdk_workshop/py_cdk_workshop_stack.py
+++ py_cdk_workshop/py_cdk_workshop_stack.py
@@ -3,24 +3,33 @@ from pyclbr import Function
 from constructs import Construct
 from aws_cdk import (
     Stack,
-   aws_lambda as _lambda,
-   aws_apigateway as apigw,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw,
 )
+
+from .hitcounter import HitCounter
 
 
 class PyCdkWorkshopStack(Stack):
-
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         my_lambda = _lambda.Function(
-            self, 'HelloHandler',
-            runtime = _lambda.Runtime.PYTHON_3_9,
-            code = _lambda.Code.from_asset('lambda'),
-            handler = 'hello.handler',
-            )
+            self,
+            "HelloHandler",
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            code=_lambda.Code.from_asset("lambda"),
+            handler="hello.handler",
+        )
 
         apigw.LambdaRestApi(
-            self, 'Endpoint',
-            handler = my_lambda,
-            )
+            self,
+            "Endpoint",
+            handler=hello_with_counter._handler,
+        )
+
+        hello_with_counter = HitCounter(
+            self,
+            "HitCountHandler",
+            downstream=my_lambda,
+        )
