fb2fa10014cc49ab474ae737372e8f92ac5d5fca
ChrisABrown <punkystylin@yahoo.com>
2022-11-30 13:31:11 -0600

    clean up

--- lambda/hello.py
+++ lambda/hello.py
@@ -1,5 +1,6 @@
 from asyncio import events
 import json
+<<<<<<< HEAD
 from urllib import request
 
 
@@ -10,3 +11,17 @@ def handler(event, context):
         "headers": {"Content-Type": "text/plain"},
         "body": "Hello, CDK! You have hit {}\n".format(event["path"]),
     }
+=======
+from urllib import request 
+
+
+def handler(event, context)
+    print('request: { }' .format(json.dumps(event))
+          return {
+              'statusCode': 200,
+              'headers': {
+                  'Content-Type': 'text/plain'
+                  },
+              'body': 'Hello, CDK! You have hit {}/n' .format(event['path'])
+          }
+>>>>>>> 3ce3f16 (Added all directores and files needed to edit code)
--- py_cdk_workshop/py_cdk_workshop_stack.py
+++ py_cdk_workshop/py_cdk_workshop_stack.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from constructs import Construct
 from aws_cdk import (
     Stack,
@@ -31,3 +32,25 @@ class PyCdkWorkshopStack(Stack):
             "Endpoint",
             handler=hello_with_counter._handler,
         )
+=======
+from cgitb import handler
+from pyclbr import Function
+from constructs import Construct
+from aws_cdk import (
+    Stack,
+   aws_lambda as _lambda,
+)
+
+
+class PyCdkWorkshopStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        my_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime = _lambda.Runtime.PYTHON_3_9,
+            code = _lambda.Code.from_asset('lambda'),
+            handler = 'hello.handler',
+            )
+>>>>>>> 3ce3f16 (Added all directores and files needed to edit code)
