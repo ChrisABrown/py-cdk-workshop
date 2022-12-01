ea5fec56673b20859dc2e98c28b22e473dde40eb
ChrisABrown <punkystylin@yahoo.com>
2022-12-01 08:49:29 -0600

    added hit count back to lambda folder

--- lambda/hello.py
+++ lambda/hello.py
@@ -1,7 +1,5 @@
-from asyncio import events
+
 import json
-<<<<<<< HEAD
-from urllib import request
 
 
 def handler(event, context):
@@ -11,17 +9,4 @@ def handler(event, context):
         "headers": {"Content-Type": "text/plain"},
         "body": "Hello, CDK! You have hit {}\n".format(event["path"]),
     }
-=======
-from urllib import request 
 
-
-def handler(event, context)
-    print('request: { }' .format(json.dumps(event))
-          return {
-              'statusCode': 200,
-              'headers': {
-                  'Content-Type': 'text/plain'
-                  },
-              'body': 'Hello, CDK! You have hit {}/n' .format(event['path'])
-          }
->>>>>>> 3ce3f16 (Added all directores and files needed to edit code)
